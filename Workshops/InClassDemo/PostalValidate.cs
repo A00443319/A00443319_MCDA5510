e10bc2b78649961ef1406cd33353f825538d8843
A00443319 <Akshaykumar.Mashalkar@smu.ca>
2021-03-12 12:44:04 -0400

    Forked InClassDemo from Dan's repo

---
+++ Workshops/InClassDemo/PostalValidate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace CustomerPortal
+{
+    class PostalValidate : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value,
+                            ValidationContext validationContext)
+        {
+
+            string str = value.ToString();
+
+            if (!IsUSorCanadianZipCode(str))
+            {
+                return new ValidationResult("Invalid postal code");
+            }
+            
+            return ValidationResult.Success;
+        }
+
+        private bool IsUSorCanadianZipCode(string zipCode)
+        {
+            bool isValidUsOrCanadianZip = false;
+            string pattern = @"^\d{5}-\d{4}|\d{5}|[A-Z]\d[A-Z] \d[A-Z]\d$";
+            Regex regex = new Regex(pattern);
+            return isValidUsOrCanadianZip = regex.IsMatch(zipCode);
+        }
+
+    }
+
+
+}
